806cb5acd2a6cbb9bab73522df31cb0dda817596
hu-coder <57177307+hu-coder@users.noreply.github.com>
2021-06-04 08:56:39 +0800

    Delete maven_mysql directory

--- maven_mysql/src/main/java/com/szu/dao/inter/ItemDao.java
+++
@@ -1,9 +0,0 @@
-package com.szu.dao.inter;
-
-import java.io.IOException;
-import java.util.List;
-import com.szu.domain.Item;
-
-public interface ItemDao {
-    public List<Item> findAll() throws Exception;
-}
